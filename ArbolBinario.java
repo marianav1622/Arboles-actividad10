7a699104aa4c34e9af23b7724b39699e4290a5c9
Mariana <mvreyesp@libertadores.edu.co>
2026-04-27 17:52:01 +0000

    insertar

--- ArbolBinario.java
+++ ArbolBinario.java
@@ -4,3 +4,18 @@ public class ArbolBinario {
     public ArbolBinario() {
         this.raiz = null;
     }
+    public void insertar(int valor) {
+        raiz = insertarRecursivo(raiz, valor);
+    }
+
+    private Nodo insertarRecursivo(Nodo actual, int valor) {
+        if (actual == null) {
+            return new Nodo(valor); 
+        }
+        if (valor < actual.valor) {
+            actual.izquierdo = insertarRecursivo(actual.izquierdo, valor);
+        } else if (valor > actual.valor) {
+            actual.derecho = insertarRecursivo(actual.derecho, valor);
+        }
+        return actual;
+    }
