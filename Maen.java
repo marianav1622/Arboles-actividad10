da762d58c236f5ba2faa2dbc2229ea0a10ac1489
Mariana <mvreyesp@libertadores.edu.co>
2026-04-27 18:01:21 +0000

    al ar

--- Maen.java
+++ Maen.java
@@ -15,4 +15,8 @@ public static void main(String[] args) {
         System.out.println("\n=== MÉTRICAS Y TERMINOLOGÍA ===");
         System.out.println("Altura del árbol: " + arbol.getAltura());
         System.out.println("Cantidad de hojas: " + arbol.contarHojas());
-        
+        System.out.println("\n=== RECORRIDO DEL ÁRBOL ===");
+        System.out.print("Recorrido Inorden: ");
+        arbol.recorrerInorden(arbol.raiz);
+        System.out.println();
+    }
