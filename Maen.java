3734be23afec6c3b0ebe13a12affe28190750aa4
Mariana <mvreyesp@libertadores.edu.co>
2026-04-27 17:59:26 +0000

    ar

---
+++ Maen.java
@@ -0,0 +1,18 @@
+public static void main(String[] args) {
+        ArbolBinario arbol = new ArbolBinario();
+         // Inserción ordenada según las reglas del ABB
+        arbol.insertar(50);
+        arbol.insertar(30);
+        arbol.insertar(70);
+        arbol.insertar(20);
+        arbol.insertar(40);
+        arbol.insertar(60);
+        arbol.insertar(80);
+
+        System.out.println("=== REPRESENTACIÓN GRÁFICA DEL ÁRBOL ===");
+        arbol.mostrarArbol();
+
+        System.out.println("\n=== MÉTRICAS Y TERMINOLOGÍA ===");
+        System.out.println("Altura del árbol: " + arbol.getAltura());
+        System.out.println("Cantidad de hojas: " + arbol.contarHojas());
+        
